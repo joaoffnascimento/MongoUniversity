8c927bf77b9f8a04fc569fb3b8c0f4142911d31c
thecurrentuser <thecurrentuser@live.com>
2020-04-09 21:30:02 -0300

    Repository - CRUD

--- UsingMongoDbWithNodeJS/app.js
+++ UsingMongoDbWithNodeJS/app.js
@@ -13,10 +13,61 @@ console.log(url)
 async function main() {
     const client = new MongoClient(url)
     await client.connect()
-    console.log('CONECTOU')
 
-    const results = await circulationRepo.loadData(data)
-    console.log(results.insertedCount, results.ops)
+    try {
+        console.log('CONECTOU')
+
+        //const results = await circulationRepo.loadData(data)
+        //assert.equal(data.lenght, results.lenght)
+
+        const getData = await circulationRepo.get()
+        assert.equal(data.lenght, getData.lenght)
+
+        const filterData = await circulationRepo.get({ Newspaper: getData[4].Newspaper })
+        assert.deepEqual(getData[4], filterData[0])
+
+        const limitData = await circulationRepo.get({}, 3);
+        assert.equal(limitData.length, 3);
+
+        const id = getData[4]._id.toString()
+        const byId = await circulationRepo.getById(id)
+        assert.deepEqual(byId, getData[4])
+
+        let newItem = {
+            "Newspaper": "Jornal Hoje Brasil",
+            "Daily Circulation, 2004": 1,
+            "Daily Circulation, 2013": 2,
+            "Change in Daily Circulation, 2004-2013": 100,
+            "Pulitzer Prize Winners and Finalists, 1990-2003": 0,
+            "Pulitzer Prize Winners and Finalists, 2004-2004": 0,
+            "Pulitzer Prize Winners and Finalists, 1990-2004": 0
+        }
+        const addedItem = await circulationRepo.addItem(newItem)
+        assert(addedItem._id)
+
+        const updateItem = await circulationRepo.update(addedItem._id, {
+            "Newspaper": "Jornal Ontem Brasil",
+            "Daily Circulation, 2004": 1,
+            "Daily Circulation, 2013": 2,
+            "Change in Daily Circulation, 2004-2013": 100,
+            "Pulitzer Prize Winners and Finalists, 1990-2003": 0,
+            "Pulitzer Prize Winners and Finalists, 2004-2004": 0,
+            "Pulitzer Prize Winners and Finalists, 1990-2004": 0
+        })
+        assert.equal(updateItem.Newspaper, "Jornal Ontem Brasil")
+
+        const removed = await circulationRepo.remove(updateItem._id)
+        assert(removed)
+
+        console.log(updateItem)
+
+    } catch (error) {
+        console.error(error)
+    } finally {
+        //await client.db(process.env.MONGODB_DATABASE).dropCollection(process.env.COLLECTION)
+        client.close()
+        console.log('BANCO DROP & DESCONECTOU')
+    }
 }
 
 main()
--- UsingMongoDbWithNodeJS/repository/circulationRepo.js
+++ UsingMongoDbWithNodeJS/repository/circulationRepo.js
@@ -1,25 +1,141 @@
-const { MongoClient } = require('mongodb')
+const { MongoClient, ObjectID } = require('mongodb')
 require('dotenv/config')
 
 function circulationRepo() {
-    //Build a circulation object
-    function loadData(data) {
+    function remove(id) {
         return new Promise(async (resolve, reject) => {
-            const client = new MongoClient(process.env.MONGODB_URL)
+            const client = MongoClient(process.env.MONGODB_URL)
+
             try {
                 await client.connect()
                 const db = client.db(process.env.MONGODB_DATABASE)
 
-                results = await db.collection('newspapers').insertMany(data)
-                
-                resolve(results)
+                const result = await db.collection(process.env.COLLECTION).deleteOne({ _id: ObjectID(id) })
+                resolve(result.deletedCount === 1)
+                client.close()
             } catch (error) {
                 reject(error)
             }
         })
     }
 
-    return { loadData }
+    function update(id, newItem) {
+        return new Promise(async (resolve, reject) => {
+            const client = MongoClient(process.env.MONGODB_URL)
+
+            try {
+                await client.connect()
+                const db = client.db(process.env.MONGODB_DATABASE)
+
+                const result = await db.collection(process.env.COLLECTION)
+                    .findOneAndReplace({ _id: ObjectID(id) }, newItem, { returnOriginal: false })
+                resolve(result.value)
+
+                client.close()
+            } catch (error) {
+                reject(error)
+            }
+        })
+    }
+
+    function addItem(item) {
+        return new Promise(async (resolve, reject) => {
+            const client = MongoClient(process.env.MONGODB_URL)
+
+            try {
+                await client.connect()
+                const db = client.db(process.env.MONGODB_DATABASE)
+
+                const result = await db.collection(process.env.COLLECTION).insertOne(item)
+                resolve(result.ops[0])
+
+                client.close()
+            } catch (error) {
+                reject(error)
+            }
+        })
+    }
+
+    function getById(id) {
+        return new Promise(async (resolve, reject) => {
+            const client = MongoClient(process.env.MONGODB_URL)
+            try {
+                await client.connect()
+                const db = client.db(process.env.MONGODB_DATABASE)
+
+                const result = await db.collection(process.env.COLLECTION).findOne({ _id: ObjectID(id) })
+
+                resolve(await result)
+
+                client.close()
+            } catch (error) {
+                reject(error)
+            }
+        })
+    }
+
+    function get(query, limit) {
+        return new Promise(async (resolve, reject) => {
+            const client = new MongoClient(process.env.MONGODB_URL)
+            try {
+
+                await client.connect()
+                const db = client.db(process.env.MONGODB_DATABASE)
+
+                let items = db.collection(process.env.COLLECTION).find(query)
+
+                if (limit > 0) {
+                    items = items.limit(limit)
+                }
+                resolve(await items.toArray())
+
+                client.close()
+            } catch (error) {
+                reject(error)
+            }
+        })
+    }
+
+    function loadData(data) {
+        return new Promise(async (resolve, reject) => {
+            const client = new MongoClient(process.env.MONGODB_URL)
+            try {
+
+                await client.connect()
+                const db = client.db(process.env.MONGODB_DATABASE)
+
+                results = await db.collection(process.env.COLLECTION).insertMany(data)
+
+                resolve(results)
+                client.close()
+            } catch (error) {
+                reject(error)
+            }
+        })
+    }
+
+    return { loadData, get, getById, addItem, update, remove }
 }
 
 module.exports = circulationRepo()
+
+// collection.find({}).project({ a: 1 })                             // Create a projection of field a
+// collection.find({}).skip(1).limit(10)                          // Skip 1 and limit 10
+// collection.find({}).batchSize(5)                               // Set batchSize on cursor to 5
+// collection.find({}).filter({ a: 1 })                              // Set query on the cursor
+// collection.find({}).comment('add a comment')                   // Add a comment to the query, allowing to correlate queries
+// collection.find({}).addCursorFlag('tailable', true)            // Set cursor as tailable
+// collection.find({}).addCursorFlag('oplogReplay', true)         // Set cursor as oplogReplay
+// collection.find({}).addCursorFlag('noCursorTimeout', true)     // Set cursor as noCursorTimeout
+// collection.find({}).addCursorFlag('awaitData', true)           // Set cursor as awaitData
+// collection.find({}).addCursorFlag('exhaust', true)             // Set cursor as exhaust
+// collection.find({}).addCursorFlag('partial', true)             // Set cursor as partial
+// collection.find({}).addQueryModifier('$orderby', { a: 1 })        // Set $orderby {a:1}
+// collection.find({}).max(10)                                    // Set the cursor max
+// collection.find({}).maxTimeMS(1000)                            // Set the cursor maxTimeMS
+// collection.find({}).min(100)                                   // Set the cursor min
+// collection.find({}).returnKey(10)                              // Set the cursor returnKey
+// collection.find({}).setReadPreference(ReadPreference.PRIMARY)  // Set the cursor readPreference
+// collection.find({}).showRecordId(true)                         // Set the cursor showRecordId
+// collection.find({}).sort([['a', 1]])                           // Sets the sort order of the cursor query
+// collection.find({}).hint('a_1')                                // Set the cursor hint
